2523a60967382b5f1ca4294a888f0db3bafca3f3
出井秀行 <gushwell@outlook.com>
2018-11-12 10:15:40 +0900

    1次元セルオートマトンルール 0からルール255まで

---
+++ CellAutomata/EasyCanvas.cs
@@ -0,0 +1,34 @@
+﻿using NGraphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CellAutomata
+{
+    class EasyCanvas
+    {
+        private IImageCanvas canvas;
+
+        static EasyCanvas()
+        {
+            // .NET Coreのパッケージの場合は、このコードが必要？
+            //Platforms.SetPlatform<SystemDrawingPlatform>();
+        }
+        public EasyCanvas(int width, int height)
+        {
+            canvas = Platforms.Current.CreateImageCanvas(new Size(width, height), scale: 2);
+        }
+
+        public void SetPixel(int x, int y, SolidBrush brush)
+        {
+            canvas.FillRectangle(x, y, 1, 1, brush);
+        }
+
+        public void Write(string path)
+        {
+            canvas.GetImage().SaveAsPng(path);
+        }
+    }
+}
---
+++ CellAutomata/Program.cs
@@ -0,0 +1,39 @@
+﻿using NGraphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CellAutomata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 256; i++)
+            {
+                int rule = i;
+                int generations = 180;
+                int width = generations * 2;
+                var ca = new CellularAutomaton(rule, width);
+                var lines = ca.AllGenerations().Take(generations);
+                MakeImage(rule, lines, width);
+            }
+        }
+
+        static void MakeImage(int rule, IEnumerable<bool[]> lines, int width)
+        {
+            var canvas = new EasyCanvas(width, width / 2 + 10);
+            int y = 10;
+            foreach (var line in lines)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    if (line[x])
+                        canvas.SetPixel(x, y, Brushes.DarkGray);
+                }
+                y++;
+            }
+            canvas.Write($"Rule{rule}.png");
+        }
+    }
+}
