a40e638bf53e53d0b2aabff70ceb8a163a59161d
出井秀行 <gushwell@outlook.com>
2018-11-12 15:08:47 +0900

    ヒルベルト曲線

---
+++ HilbertCurve/Drawer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HilbertCurve
+{
+    class Drawer : IObserver<DrawInfo>
+    {
+        private EasyCanvas _canvas;
+
+        private string _filepath;
+
+        public Drawer(int w, int h, string filepath)
+        {
+            _canvas = new EasyCanvas(w, h);
+            _filepath = filepath;
+        }
+        public void OnCompleted()
+        {
+            _canvas.Write(_filepath);
+        }
+
+        public void OnError(Exception error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNext(DrawInfo value)
+        {
+            _canvas.DrawLine(value.Start.X, value.Start.Y, value.End.X, value.End.Y, 
+                new NGraphics.Color("#0022FF"));
+        }
+    }
+}
---
+++ HilbertCurve/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HilbertCurve
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int generation = 6;
+            int w = 500;
+            var hilbert = new HilbertCurve();
+            var drawer = new Drawer(w, w, $"HilbertCurve{generation}.png");
+            hilbert.Subscribe(drawer);
+            hilbert.Start(w, generation);
+        }
+    }
+}
